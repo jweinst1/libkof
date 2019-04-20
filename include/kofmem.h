7b2919af2e6e899a72262f857497349980221ce8
Joshua Weinstein <jweinstein@splunk.com>
2019-04-19 23:54:53 -0700

    mem test work

--- include/kofmem.h
+++ include/kofmem.h
@@ -15,30 +15,28 @@
  * via stdout.
  */
 #ifdef KOF_MEMORY_DEBUG_MODE
-#  define kof_malloc(mem_size) (malloc(mem_size) + (0 * printf("KOFMEM - (malloc) size:%lu\n", mem_size)))
-#  define kof_calloc(mem_size) (calloc(mem_size, 0) + (0 * printf("KOFMEM - (calloc) size:%lu\n", mem_size)))
+#  define kof_malloc(mem_size) (malloc(mem_size) + (0 * printf("KOFMEM - (malloc) size:%lu, line:%u, file:%s\n", (unsigned long)mem_size, __LINE__, __FILE__)))
+#  define kof_calloc(mem_size) (calloc(mem_size, 0) + (0 * printf("KOFMEM - (calloc) size:%lu, line:%u, file:%s\n", (unsigned long)mem_size, __LINE__, __FILE__)))
 #  define kof_realloc(mem_ptr, mem_size) \
-              (realloc(mem_ptr, mem_size) + (0 * printf("KOFMEM - (realloc) size:%lu, addr:%p\n\n", mem_size, mem_ptr)))
-#  define kof_free(mem_ptr) (free(mem_ptr) + (0 * printf("KOFMEM - (free) addr:%p\n", mem_ptr)))
+              (realloc(mem_ptr, mem_size) + (0 * printf("KOFMEM - (realloc) size:%lu, addr:%p, line:%u, file:%s\n", mem_size, mem_ptr, __LINE__, __FILE__)))
+#  define kof_free(mem_ptr) free(mem_ptr); printf("KOFMEM - (free) addr:%p, line:%u, file:%s\n", mem_ptr, __LINE__, __FILE__);
 #else
 #  define kof_malloc(mem_size) (malloc(mem_size))
 #  define kof_realloc(mem_ptr, mem_size) (realloc(mem_ptr, mem_size))
 #  define kof_calloc(mem_size) (calloc(mem_size, 0)) 
-#  define kof_free(mem_ptr) (free(mem_ptr))
+#  define kof_free(mem_ptr) free(mem_ptr)
 #endif
 
-#ifndef KOF_MEMORY_POOL_START
-#define KOF_MEMORY_POOL_START 2056
-#endif
-
-typedef struct __kof_mem_pool
-{
-	void* start;
-	void* cur;
-	void* end;
-} kof_mem_pool_t;
-
-
-#define kof_mem_pool_declare(name) static kof_mem_pool_t name 
+/*Exit code used for NULL on alloc*/
+#define KOF_MEMORY_OMM_EXIT 3
+/**
+ * Creates a clean string with room for a '\0'
+ * Must be freed when done.
+ */
+#define kof_mem_str(size) ((char)kof_calloc(size + 1))
+/**
+ * Same as above but is not cleaned memory.
+ */
+#define kof_mem_raw_str(size) ((char)kof_malloc(size + 1))
 
 #endif // KOF_MEM_H
---
+++ utils/tests/test_kofmem.c
@@ -0,0 +1,15 @@
+#include "kofmem.h"
+#include "test-tools.h"
+
+static void test_kof_calloc(void)
+{
+	unsigned char* membyt = kof_calloc(10);
+	TEST_CHECK_POINT(membyt[0] == 0);
+	kof_free(membyt);
+}
+
+int main(int argc, char const *argv[])
+{
+	test_kof_calloc();
+	return 0;
+}
