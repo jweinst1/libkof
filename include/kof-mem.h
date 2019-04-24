06608a02fcef192b0b18caee90e85449eb09a7f5
Joshua Weinstein <jweinstein@splunk.com>
2019-04-24 09:20:09 -0700

    license in config header

--- include/kof-mem.h
+++ include/kof-mem.h
@@ -33,10 +33,39 @@
  * Creates a clean string with room for a '\0'
  * Must be freed when done.
  */
-#define kof_mem_str(size) ((char)kof_calloc(size + 1))
+#define kof_mem_ch_str(size) ((char)kof_calloc(size + 1))
 /**
  * Same as above but is not cleaned memory.
  */
 #define kof_mem_raw_str(size) ((char)kof_malloc(size + 1))
+
+
+typedef struct __kof_mem_pool
+{
+	void* begin;
+	void* cur;
+	void* end;
+} kof_mem_pool_t;
+
+#define KOF_MEM_POOL_SPACE(pool) (pool.end - pool.cur)
+#define KOF_MEM_POOL_LEN(pool) (pool.cur - pool.begin)
+#define KOF_MEM_POOL_CAP(pool) (pool.end - pool.begin)
+#define KOF_MEM_POOL_FULL(pool) (pool.end == pool.cur)
+/**
+ * Macro initializer for embedded mem pool
+ * Does not clean, must be explicitly cleaned.
+ */
+#define KOF_MEM_POOL_INIT(pool, init_size) \
+                           pool.begin = kof_malloc(init_size); \
+                           pool.cur = pool.begin; \
+                           pool.end = pool.begin + init_size
+
+#define KOF_MEM_POOL_GROW(pool) do { \
+	size_t old_size = pool.end - pool.begin; \
+	size_t old_len = pool.cur - pool.begin; \
+	pool.begin = realloc(pool.begin, old_size * old_size); \
+	pool.cur = poo.begin + old_len; \
+	pool.end = pool.begin + (old_size * old_size); \
+} while (0)
 
 #endif // KOF_UTILS_MEM_H
